658d05c44c23e4ba6f2ea1fab399070e86c737b5
Burutsako24 <77276546+Burutsako24@users.noreply.github.com>
2021-03-26 00:12:01 +0700

    Add files via upload

---
+++ ImageProductAPI.php
@@ -0,0 +1,128 @@
+<?php
+   
+require APPPATH . 'libraries/REST_Controller.php';
+     
+class ImageProductAPI extends REST_Controller {
+    public $dada;
+    
+	  /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function __construct() {
+       parent::__construct();
+       $this->load->database();
+    }
+       
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+	public function index_get()
+	{
+        try {
+            $this->load->model('ImageProductModel');
+            $this->load->helper("security"); 
+            $stream = $this->security->xss_clean( $this->input->raw_input_stream );
+            $data = json_decode(trim($stream), true);
+            if(!empty($data["ID"])){
+                $id = $data["ID"];
+                $data = $this->ImageProductModel->getbyID($id);
+            }else{
+                $data = $this->ImageProductModel->getAll();
+            }
+            $msg = array("Status"=>"success", "Message"=>"","data"=>$data);
+            $this->response($msg, REST_Controller::HTTP_OK);
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+            $msg = array("Status"=>"error", "Message"=>$e->getMessage());
+            $this->response($msg, REST_Controller::HTTP_OK);
+        }
+        
+	}
+      
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_post()
+    {
+        try {
+            $this->load->model('SchoolModel');
+            $this->load->helper("security"); 
+            $stream = $this->security->xss_clean( $this->input->raw_input_stream );
+            $data = json_decode(trim($stream), true);
+            if(!empty($data["SID"]) && !empty($data["Name"])){
+                $ststus = $this->SchoolModel->insert($data);
+                $msg = array("Status"=>$ststus["Status"], "Message"=>$ststus["Message"],"data"=>array());
+            }else{
+                $msg = array("Status"=>"error", "Message"=>"Data is not complete","data"=>array());
+            }
+            $this->response($msg, REST_Controller::HTTP_OK);
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+            $msg = array("Status"=>"error", "Message"=>$e->getMessage());
+            $this->response($msg, REST_Controller::HTTP_OK);
+        }
+    } 
+     
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_put()
+    {
+        try {
+            $this->load->model('SchoolModel');
+            $this->load->helper("security"); 
+            $stream = $this->security->xss_clean( $this->input->raw_input_stream );
+            $data = json_decode(trim($stream), true);
+            if(!empty($data["SID"]) && !empty($data["Name"])){
+                $ststus = $this->SchoolModel->update($data);
+                $msg = array("Status"=>$ststus["Status"], "Message"=>$ststus["Message"],"data"=>array());
+            }else{
+                $msg = array("Status"=>"error", "Message"=>"Data is not complete","data"=>array());
+            }
+            $this->response($msg, REST_Controller::HTTP_OK);
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+            $msg = array("Status"=>"error", "Message"=>$e->getMessage());
+            $this->response($msg, REST_Controller::HTTP_OK);
+        }
+    }
+     
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_delete()
+    {
+        try {
+            $this->load->model('SchoolModel');
+            $this->load->helper("security"); 
+            $stream = $this->security->xss_clean( $this->input->raw_input_stream );
+            $data = json_decode(trim($stream), true);
+            if(!empty($data["SID"])){
+                $ststus = $this->SchoolModel->delete($data);
+                $msg = array("Status"=>$ststus["Status"], "Message"=>$ststus["Message"],"data"=>array());
+            }else{
+                $msg = array("Status"=>"error", "Message"=>"Data is not complete","data"=>array());
+            }
+            $this->response($msg, REST_Controller::HTTP_OK);
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+            $msg = array("Status"=>"error", "Message"=>$e->getMessage());
+            $this->response($msg, REST_Controller::HTTP_OK);
+        }
+    }
+    	
+}
